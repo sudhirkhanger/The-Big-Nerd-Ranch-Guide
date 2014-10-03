6650ed1c114361a25d0ff69966b9c35547604536
Sudhir Khanger <sudhir@sudhirkhanger.com>
2014-10-04 01:21:32 +0530

    Starting An Activity pg98

---
+++ GeoQuiz/src/com/sudhirkhanger/app/geoquiz/CheatActivity.java
@@ -0,0 +1,14 @@
+package com.sudhirkhanger.app.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheatActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cheat);
+	}
+
+}
