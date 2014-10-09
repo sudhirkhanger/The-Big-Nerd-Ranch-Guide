855d91467fc20b1244545e1d1222ff4c8b782403
Sudhir Khanger <sudhir@sudhirkhanger.com>
2014-10-09 22:08:57 +0530

    pg175

--- CriminalIntent/src/com/sudhirkhanger/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/src/com/sudhirkhanger/android/criminalintent/CrimeActivity.java
@@ -10,7 +10,7 @@ public class CrimeActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_crime);
+		setContentView(R.layout.activity_fragment);
 
 		FragmentManager fm = getSupportFragmentManager();
 		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
@@ -20,17 +20,4 @@ public class CrimeActivity extends FragmentActivity {
 					.commit();
 		}
 	}
-
-	/*
-	 * @Override public boolean onCreateOptionsMenu(Menu menu) { // Inflate the
-	 * menu; this adds items to the action bar if it is present.
-	 * getMenuInflater().inflate(R.menu.crime, menu); return true; }
-	 * 
-	 * @Override public boolean onOptionsItemSelected(MenuItem item) { // Handle
-	 * action bar item clicks here. The action bar will // automatically handle
-	 * clicks on the Home/Up button, so long // as you specify a parent activity
-	 * in AndroidManifest.xml. int id = item.getItemId(); if (id ==
-	 * R.id.action_settings) { return true; } return
-	 * super.onOptionsItemSelected(item); }
-	 */
 }
