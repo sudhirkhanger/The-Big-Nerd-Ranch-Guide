3d17281f5548185eab447866096942044cb1894a
Sudhir Khanger <sudhir@sudhirkhanger.com>
2014-10-09 22:10:05 +0530

    pg175

---
+++ CriminalIntent/src/com/sudhirkhanger/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,13 @@
+package com.sudhirkhanger.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public abstract class CrimeListActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
