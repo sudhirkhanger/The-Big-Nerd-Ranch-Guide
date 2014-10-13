9d348ec8e8af298cc2624758bbc086bae1fb8fde
Sudhir Khanger <sudhir@sudhirkhanger.com>
2014-10-13 13:21:38 +0530

    CriminalIntent/AndroidManifest.xml

--- CriminalIntent/src/com/sudhirkhanger/android/criminalintent/CrimeListFragment.java
+++ CriminalIntent/src/com/sudhirkhanger/android/criminalintent/CrimeListFragment.java
@@ -30,7 +30,7 @@ public class CrimeListFragment extends ListFragment {
 		Crime c = ((CrimeAdapter) getListAdapter()).getItem(position);
 
 		// Start CrimeActivity
-		Intent i = new Intent(getActivity(), CrimeActivity.class);
+		Intent i = new Intent(getActivity(), CrimePagerActivity.class);
 		i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
 		startActivity(i);
 
