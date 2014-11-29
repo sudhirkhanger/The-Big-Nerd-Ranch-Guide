223c98c6eaababfde9e3e121f7f713e168fb2cf1
Sudhir Khanger <sudhir@sudhirkhanger.com>
2014-11-29 09:10:37 +0530

    Chapter 13 Audio Playback Using MediaPlayer

--- HelloMoon/src/com/sudhirkhanger/android/hellomoon/HelloMoonActivity.java
+++ HelloMoon/src/com/sudhirkhanger/android/hellomoon/HelloMoonActivity.java
@@ -1,11 +1,11 @@
 package com.sudhirkhanger.android.hellomoon;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class HelloMoonActivity extends ActionBarActivity {
+public class HelloMoonActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
