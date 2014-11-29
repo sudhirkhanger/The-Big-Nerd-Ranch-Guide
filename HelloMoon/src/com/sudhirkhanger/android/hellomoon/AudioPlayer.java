97aa58e37002b976bbdc5300cbb2ef77591d1844
Sudhir Khanger <sudhir@sudhirkhanger.com>
2014-11-29 09:12:27 +0530

    pg 236

---
+++ HelloMoon/src/com/sudhirkhanger/android/hellomoon/AudioPlayer.java
@@ -0,0 +1,32 @@
+package com.sudhirkhanger.android.hellomoon;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+public class AudioPlayer {
+
+	private MediaPlayer mPlayer;
+
+	public void stop() {
+		if (mPlayer != null) {
+			mPlayer.release();
+			mPlayer = null;
+		}
+	}
+
+	public void play(Context c) {
+		stop();
+
+		mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
+
+		mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+			public void onCompletion(MediaPlayer mp) {
+				stop();
+			}
+		});
+
+		mPlayer.start();
+
+	}
+
+}
---
+++ HelloMoon/src/com/sudhirkhanger/android/hellomoon/HelloMoonFragment.java
@@ -0,0 +1,43 @@
+package com.sudhirkhanger.android.hellomoon;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class HelloMoonFragment extends Fragment {
+
+	private AudioPlayer mPlayer = new AudioPlayer();
+	private Button mPlayButton;
+	private Button mStopButton;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_hello_moon, parent, false);
+		mPlayButton = (Button) v.findViewById(R.id.hellomoon_playButton);
+		mPlayButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				mPlayer.play(getActivity());
+			}
+		});
+
+		mStopButton = (Button) v.findViewById(R.id.hellomoon_stopButton);
+		mStopButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				mPlayer.stop();
+			}
+		});
+
+		return v;
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mPlayer.stop();
+	}
+
+}
