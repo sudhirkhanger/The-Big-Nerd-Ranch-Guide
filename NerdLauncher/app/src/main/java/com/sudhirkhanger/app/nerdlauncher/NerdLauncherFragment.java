2ae27a8d3b51011857825aace4fbd7a537d8cefd
Sudhir Khanger <sudhir@sudhirkhanger>
2015-01-18 11:49:45 +0530

    Chapter 23 More About Intents and Tasks pg 385 Nerd Launcher finished

--- NerdLauncher/app/src/main/java/com/sudhirkhanger/app/nerdlauncher/NerdLauncherFragment.java
+++ NerdLauncher/app/src/main/java/com/sudhirkhanger/app/nerdlauncher/NerdLauncherFragment.java
@@ -1,12 +1,20 @@
 package com.sudhirkhanger.app.nerdlauncher;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -27,7 +35,47 @@ public class NerdLauncherFragment extends ListFragment {
         startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
         PackageManager pm = getActivity().getPackageManager();
         List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
         Log.i(TAG, "I've found " + activities.size() + " activities.");
+
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            public int compare(ResolveInfo a, ResolveInfo b) {
+                PackageManager pm = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        a.loadLabel(pm).toString(),
+                        b.loadLabel(pm).toString());
+            }
+        });
+
+        ArrayAdapter<ResolveInfo> adapter = new ArrayAdapter<ResolveInfo>(
+                getActivity(), android.R.layout.simple_list_item_1, activities) {
+            public View getView(int pos, View convertView, ViewGroup parent) {
+                PackageManager pm = getActivity().getPackageManager();
+                View v = super.getView(pos, convertView, parent);
+                // Documentation says that simple_list_item_1 is a TextView,
+                // so cast it so that you can set its text value
+                TextView tv = (TextView) v;
+                ResolveInfo ri = getItem(pos);
+                tv.setText(ri.loadLabel(pm));
+                return v;
+            }
+        };
+        setListAdapter(adapter);
+
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        ResolveInfo resolveInfo = (ResolveInfo) l.getAdapter().getItem(position);
+        ActivityInfo activityInfo = resolveInfo.activityInfo;
+
+        if (activityInfo == null) return;
+
+        Intent i = new Intent(Intent.ACTION_MAIN);
+        i.setClassName(activityInfo.applicationInfo.packageName, activityInfo.name);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        startActivity(i);
     }
 
 }
